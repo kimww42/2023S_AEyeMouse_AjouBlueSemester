5efa6c2b55d955e2804e2135fb05ebe41dd22158
swkim <kimsiw42@ajou.ac.kr>
2023-04-10 22:19:30 +0900

    fix

--- AI/main.py
+++ AI/main.py
@@ -2,9 +2,6 @@
 virtualenv 환경에서 dlib importerror가 나는 관계로
 conda 환경에서 실행.
 """
-import argparse
-import collections
-
 import cv2
 import dlib
 import imutils
@@ -17,6 +14,7 @@ detector = dlib.get_frontal_face_detector()
 
 # opencv의 frame_width,height를 사용하면 기본적으로 카메라에서 지원되는 해상도로만 조절이 가능
 # 그래서 imutils의 resize로 강제로 해상도를 바꿀 수 있음
+
 """
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920) # 가로
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080) # 세로
@@ -27,10 +25,11 @@ def detect(gray, frame):
 
         face_landmarks = predictor(gray, face)
 
-        for n in range(0, 68):
+        for n in range(0, 67):
             x = face_landmarks.part(n).x
             y = face_landmarks.part(n).y
-            cv2.circle(frame, (x, y), 1, (0, 0, 255), 3)
+
+            cv2.circle(frame, (x, y), 1, (0, 255, 0), 1)
 
 while True:
     # 영상을 받아오지 못할 경우, retval은 false로 return.
@@ -39,8 +38,8 @@ while True:
     frame = cv2.flip(frame, 1)
     # 해상도 변경
     frame = imutils.resize(frame, width=1920)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    detect(gray, frame)
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    detect(frame, frame)
 
     if not retval:
         print("couldn't read frames")
