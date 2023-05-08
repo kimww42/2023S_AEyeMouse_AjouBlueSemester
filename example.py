004295be5f339eabe2038f8363744b75c42c9d90
SibeDoChi <leeja2001@gmail.com>
2023-05-09 00:31:42 +0900

    merge to gui

--- example.py
+++ example.py
@@ -30,17 +30,11 @@ while True:
     #webcam에 좌우 눈동자 좌표를 text로 띄우는 부분
     left_pupil = gaze.pupil_left_coords()
     right_pupil = gaze.pupil_right_coords()
-    clink = blink.double_blink()
     
     cv2.putText(frame, "Left pupil:  " + str(left_pupil), (5, 25), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
     cv2.putText(frame, "Right pupil: " + str(right_pupil), (5, 60), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
     cv2.putText(frame, "Blink Count: " + str(count), (5, 105), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-    if clink == 0:
-        cv2.putText(frame, "Clink:" + 'no', (5, 150), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-    elif clink == 1:
-        cv2.putText(frame, "Clink:" + 'yes', (5, 150), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-        
-        
+            
     cv2.imshow("Demo", frame)  
     
     if cv2.waitKey(1) == 27:
